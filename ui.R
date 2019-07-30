6cdfa522a820894ec841ef256a436b5ba66a4d57
Beverly Dodds <beverd01@nss.scot.nhs.uk>
2019-07-30 14:42:32 +0100

    Amended type of drop-down

--- ui.R
+++ ui.R
@@ -2,43 +2,47 @@
 # User interface - how your app looks and elements users can interact with
 
 shinyUI(fluidPage(
-  
   title = "Runchart Builder",
-  
+                  
   fluidRow(
+    br(),
     column(2, "Build your chart",
-           selectInput("datatype", "Select measure", c("CAMHS seen", 
-                                                       "CAMHS waiting",
-                                                       "PT seen",
-                                                       "PT waiting"), selected = NULL, multiple = FALSE, selectize = FALSE),
-           selectInput("hb", "Select geography", c("NHS Ayrshire and Arran",
-                                                   "NHS Borders",
-                                                   "NHS Dumfries and Galloway",
-                                                   "NHS Fife",
-                                                   "NHS Forth Valley",
-                                                   "NHS Grampian",
-                                                   "NHS Greater Glasgow and Clyde", 
-                                                   "NHS Highland", 
-                                                   "NHS Lanarkshire", 
-                                                   "NHS Lothian", 
-                                                   "NHS Orkney", 
-                                                   "NHS Shetland", 
-                                                   "NHS Tayside", 
-                                                   "NHS Western Isles",
-                                                   "NHS Scotland"), selected = NULL, multiple = FALSE, selectize = FALSE)),
+    column(12,
+      pickerInput(
+        inputId = "datatype",
+        label = "Select measure",
+        choices = as.character(unique(measures$measure)),
+        width = "fit"
+      ),
+      pickerInput(
+        inputId = "hb",
+        label = "Select board",
+        choices = as.character(unique(measures$board))
+      )
+    )
+    ),
+  
+  # Show a plot of the generated distribution
     
+    column(8, offset = 0.9,
+           plotOutput("runchart")
+    )
     
-    # Show a plot of the generated distribution
-    column(8,
-           plotOutput("runchart"))
   ),
-    
-    # Show table
-    
-  fluidRow(column(10, dataTableOutput("rundata"))),
-  
-  fluidRow(column(2,downloadButton("downloaddata", "Download data")),
-  
-           column(2,downloadButton("downloadchart", "Download chart")))
+                      
+  br(),
+                      
+  # Show table
+          
+  fluidRow(column(7, dataTableOutput("rundata"))
+  ),
+                    
+  fluidRow(
+    column(2, downloadButton("downloaddata", "Download data")
+    ),
+                    
+    column(10, downloadButton("downloadchart", "Download chart")
+    )
   ))
-
+)
+        
